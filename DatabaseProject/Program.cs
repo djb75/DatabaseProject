9eb07789aad2f03c9dfcf5aba470ccbd9a2bce01
djb75 <jorgebd@stpaulsschool.org.uk>
2024-05-16 15:39:34 +0100

    Setup

--- DatabaseProject/Program.cs
+++ DatabaseProject/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace DatabaseProject
+﻿using DatabaseProject.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace DatabaseProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string dbPath = @"Data Source=C:\Users\david\Downloads\StudentOptions.db;Mode=ReadWrite";
+
+            Console.WriteLine(dbPath);
+            StudentDB db = new(dbPath); //Make db class
+
+            var names = db.GetUserNames();
+            Console.WriteLine("User Names:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a name to add:");
+            string newName = Console.ReadLine() ?? string.Empty;
+            db.AddUserName(newName);
         }
     }
 }
